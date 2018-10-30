4775f5f3b8157a2892635bef04c00a03477294bb
Rajat Shamra <rajat.sharma@xooa.com>
2018-10-30 14:37:39 +0530

    Event addition

--- contract/GetSetContract.sol
+++ contract/GetSetContract.sol
@@ -5,12 +5,15 @@ contract GetSetContract{
     string h = "hello";
     string data;
 
+    event StateSaveEvent(string data);
+
     function get() public view returns(string) {
         return data;
     }
 
     function set(string arg) public returns(string) {
-        data = arg;
+        data = arg;     // this is a transaction. There is no explicit write
+        emit StateSaveEvent(arg);
         return data;
     }
 
