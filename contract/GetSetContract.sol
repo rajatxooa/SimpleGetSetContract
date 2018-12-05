d29182fae195db11193cdd902c66f31aca29217f
Rajat Shamra <rajat.sharma@xooa.com>
2018-12-05 13:23:15 +0530

    assertion removed from get to check for key

--- contract/GetSetContract.sol
+++ contract/GetSetContract.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.0;
 
-contract GetSetContract{
+contract XooaGetSet{
 
     mapping (string => string) state;
     event StateSaveEvent(string data);
@@ -8,39 +8,10 @@ contract GetSetContract{
     function get(string memory key) public view returns(string) {
         string memory value = state[key];
         
-        require(!equal(value, ''));
-        
-        if (equal(value, 'error')) {
-            throw;
-        }
-        
         return value;
     }
 
     function set(string memory key, string memory value) public returns(string) {
         state[key] = value;
-    }
-    
-    function compare(string _a, string _b) internal returns (int) {
-        bytes memory a = bytes(_a);
-        bytes memory b = bytes(_b);
-        uint minLength = a.length;
-        if (b.length < minLength) minLength = b.length;
-        
-        for (uint i = 0; i < minLength; i ++)
-            if (a[i] < b[i])
-                return -1;
-            else if (a[i] > b[i])
-                return 1;
-        if (a.length < b.length)
-            return -1;
-        else if (a.length > b.length)
-            return 1;
-        else
-            return 0;
-    }
-    
-    function equal(string _a, string _b) internal returns (bool) {
-        return compare(_a, _b) == 0;
     }
 }
